01f709c92db4914b811959b0a833efab91b9b223
lennies <lenniesliwinski@gmail.com>
2013-12-05 12:41:43 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trainerio::Application.config.secret_key_base = 'b9c61108fab055c640d31427fffa9169d7d7d86f62788f9110943f6a8c06d162b79b380755b695a1dedd940b69cb34a1d1a301671e498986ea650b4a71b19c5a'
