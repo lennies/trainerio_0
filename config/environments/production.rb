135eafb601bcab396aa1b451141b1fded048d048
lennies <lenniesliwinski@gmail.com>
2013-12-06 13:50:15 -0500

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Trainerio::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Devise -- set to actual hostname
+  config.action_mailer.default_url_options = { :host => 'http://trainerio.herokuapp.com/' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Trainerio::Application.routes.draw do
+  devise_for :users
  root "pages#home"
  get "pages/home2"
   # The priority is based upon order of creation: first created -> highest priority.
